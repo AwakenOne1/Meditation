2615b5ead19dad8b678f3acd93b4c755024b2dc1
AwakenOne1 <alekseydubovik2003@icloud.com>
2023-12-29 11:17:25 +0300

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Alexey Dubovik on 29.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
